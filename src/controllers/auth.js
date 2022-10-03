be8413d75874ce6f80a4b1cf29760b7fea849044
EduBarrros <eduardodeoliveirabarros@gmail.com>
2022-10-03 20:09:13 -0300

    Fix: add exception handling on auth controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,25 +5,33 @@ exports.cadastrar = (req, res) => {
 
     firebase.auth().createUserWithEmailAndPassword(data.email, data.password)
     .then((userCredential) => {
-        res.status(201).send({msg: 'Usuario cadastrado com sucesso!', user: userCredential.user.email})
+        res.status(201).send({status: 1, msg: 'Usuario cadastrado com sucesso!', user: userCredential.user.email})
     })
     .catch((error) => {
-        res.status(500).send({msg: 'Algo deu errado', error: error})
+        if(error.code === 'auth/email-already-in-use'){
+            res.status(401).send({status: 0, msg: 'Usuário já cadastrado no sistema.'})
+        }else if(error.code === 'auth/weak-password'){
+            res.status(401).send({status: 0, msg: 'Senha muito fraca, a senha precisa de no mínimo 6 digitos.'})
+        }else{
+            res.status(500).send({status: 0, msg: 'Algo deu errado.', error: error})
+        }
     })
 }
 
 exports.entrar = (req, res) => {
     const data = req.body;
-    
-    if(data == {}){
-        return res.status(400).send({status: 0, msg: 'Dados incorretos'})
-    }
 
     firebase.auth().signInWithEmailAndPassword(data.email, data.password)
     .then((userCredential) => {
         res.status(200).send({status: 1, msg: 'Usuário logado com sucesso!', autenticado: true, user: userCredential.user.email})
     })
     .catch((error) => {
-        res.status(500).send({status: 0, msg: 'Algo deu errado', autendicado: false, error: error})
+        if(error.code === 'auth/wrong-password'){
+            res.status(401).send({status: 0, msg: 'Usuario ou senha incorretos.', autendicado: false})
+        }else if(error.code === 'auth/user-not-found'){
+            res.status(401).send({status: 0, msg: 'Usuario ou senha incorretos.', autendicado: false})
+        }else{
+            res.status(500).send({status: 0, msg: 'Algo deu errado.', autendicado: false, error: error})
+        }
     })
 }
