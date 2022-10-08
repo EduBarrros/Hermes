00e55ce2cd4989999e91c99e4a2ef04c8a1d5c2c
Eduardo Barros <eduardodeoliveirabarros@gmail.com>
2022-10-08 12:55:34 -0300

    Feat: added checkout valuation calc and response

--- src/controllers/parking.js
+++ src/controllers/parking.js
@@ -1,4 +1,6 @@
 const firebase = require('../config/firebase')
+const valores = require('../config/valores')
+const moment = require('moment')
 
 const db = firebase.firestore()
 const parking = db.collection('cl_parking')
@@ -10,6 +12,7 @@ exports.checkIn = async (req, res) => {
         emailFuncionario: data?.emailFuncionario,
         hrEntrada: new Date(),
         hrSaida: null,
+        valorFinal: null,
         placa: data?.placa,
         cor: data?.cor,
         isParking: true,
@@ -34,18 +37,119 @@ exports.checkIn = async (req, res) => {
 }
 
 exports.listarCheckIn = async (req, res) => {
-    const snapshot = await parking.get()
-    const listParking = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
-    const formatedDate = listParking.map((doc) => ({ ...doc, hrEntrada: new Date(doc.hrEntrada.seconds * 1000).toLocaleString('pt-BR', {
-        timeZone: 'America/Sao_Paulo'
-      })
-      }))
-    res.status(201).send(formatedDate)
+    try {
+        const snapshot = await parking.where('hrSaida', '==', null).get()
+        const listParking = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+        const formatedDate = listParking.map((doc) => ({
+            ...doc, hrEntrada: new Date(doc.hrEntrada.seconds * 1000).toLocaleString('pt-BR', {
+                timeZone: 'America/Sao_Paulo'
+            })
+        }))
+        res.status(201).send({ status: 1, data: formatedDate })
+    } catch (error) {
+        res.status(500).send({ status: 0, msg: 'Algo deu errado.', error: error })
+    }
 }
 
 exports.checkOut = async (req, res) => {
-    const id = req.params.id
-    await parking.doc(id).update({ "Cor": "Verde" })
+    try {
+        const id = req.params.id
+        const snapshot = await parking.doc(id).get()
+        const data = snapshot.data()
+        const responseDate = new Date(data.hrEntrada.seconds * 1000).toLocaleString('pt-BR', {
+            timeZone: 'America/Sao_Paulo'
+        })
+        const nowDate = new Date().toLocaleString('pt-BR', {
+            timeZone: 'America/Sao_Paulo'
+        })
+        var startDate = moment(responseDate, 'YYYY-M-DD HH:mm:ss')
+        var endDate = moment(nowDate, 'YYYY-M-DD HH:mm:ss')
+        var secondsDiff = endDate.diff(startDate, 'hours')
+        var valorFinal = null
 
-    res.status(201).send({ msg: 'Usuário atualizado com sucesso.' })
+        switch (secondsDiff) {
+            case 0:
+                valorFinal = valores[0]
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal })
+                break;
+
+            case 1:
+                valorFinal = valores[1]
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal })
+                break;
+
+            case 2:
+                valorFinal = valores[2]
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal })
+                break;
+
+            case 3:
+                valorFinal = valores[3]
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal })
+                break;
+
+            case 4:
+                valorFinal = valores[4]
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal })
+                break;
+
+            case 5:
+                valorFinal = valores[5]
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal })
+                break;
+
+            case 6:
+                valorFinal = valores[6]
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal })
+                break;
+
+            case 7:
+                valorFinal = valores[7]
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal })
+                break;
+
+            case 8:
+                valorFinal = valores[8]
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal })
+                break;
+
+            case 9:
+                valorFinal = valores[9]
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal })
+                break;
+
+            case 10:
+                valorFinal = valores[10]
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal })
+                break;
+
+            case 11:
+                valorFinal = valores[11]
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal })
+                break;
+
+            case 12:
+                valorFinal = valores[12]
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal })
+                break;
+
+            default:
+                break;
+        }
+
+        res.status(201).send({ status: 1, msg: 'Registro atualizado com sucesso.' })
+    } catch (error) {
+        res.status(500).send({ status: 0, msg: 'Algo deu errado.', error: error })
+    }
+}
+
+exports.listarCheckOut = async (req, res) => {
+    const snapshot = await parking.where('hrSaida', '!=', null).get()
+    const listParking = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+    const formatedDate = listParking.map((doc) => ({
+        ...doc, hrEntrada: new Date(doc.hrEntrada.seconds * 1000).toLocaleString('pt-BR', {
+            timeZone: 'America/Sao_Paulo'
+        })
+    }))
+    res.status(201).send(formatedDate)
 }
