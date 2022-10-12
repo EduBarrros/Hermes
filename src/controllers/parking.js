3574feddb6aaa1fbd06b43fb227c2313aa9d60da
Eduardo Barros <eduardodeoliveirabarros@gmail.com>
2022-10-12 09:15:57 -0300

    Feat:  added selectCheckIn and selectCheckOut endpoints

--- src/controllers/parking.js
+++ src/controllers/parking.js
@@ -80,71 +80,71 @@ exports.checkOut = async (req, res) => {
         switch (secondsDiff) {
             case 0:
                 valorFinal = valores[0]
-                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false})
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false })
                 break;
 
             case 1:
                 valorFinal = valores[1]
-                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false})
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false })
                 break;
 
             case 2:
                 valorFinal = valores[2]
-                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false})
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false })
                 break;
 
             case 3:
                 valorFinal = valores[3]
-                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false})
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false })
                 break;
 
             case 4:
                 valorFinal = valores[4]
-                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false})
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false })
                 break;
 
             case 5:
                 valorFinal = valores[5]
-                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false})
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false })
                 break;
 
             case 6:
                 valorFinal = valores[6]
-                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false})
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false })
                 break;
 
             case 7:
                 valorFinal = valores[7]
-                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false})
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false })
                 break;
 
             case 8:
                 valorFinal = valores[8]
-                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false})
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false })
                 break;
 
             case 9:
                 valorFinal = valores[9]
-                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false})
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false })
                 break;
 
             case 10:
                 valorFinal = valores[10]
-                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false})
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false })
                 break;
 
             case 11:
                 valorFinal = valores[11]
-                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false})
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false })
                 break;
 
             case 12:
                 valorFinal = valores[12]
-                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false})
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": valorFinal, "isParking": false })
                 break;
 
             default:
-                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": 150, "isParking": false})
+                await parking.doc(id).update({ "hrSaida": new Date(), "valorFinal": 150, "isParking": false })
                 break;
         }
 
@@ -175,7 +175,61 @@ exports.listarCheckOut = async (req, res) => {
                 cor: doc.cor
             }
         }))
-        res.status(201).send({status: 1, data: formatedDate})
+        res.status(201).send({ status: 1, data: formatedDate })
+    } catch (error) {
+        res.status(500).send({ status: 0, msg: 'Algo deu errado.', error: error })
+    }
+}
+
+exports.selectCheckIn = async (req, res) => {
+    try {
+        const id = req.params.id
+        const snapshot = await parking.doc(id).get()
+        const data = snapshot.data()
+        const formatedDate = {
+            id: id,
+            hrEntrada: new Date(data.hrEntrada.seconds * 1000).toLocaleString('pt-BR', {
+                timeZone: 'America/Sao_Paulo'
+            }),
+            hrSaida: data.hrSaida,
+            isParking: data.isParking,
+            emailFuncionario: data.emailFuncionario,
+            valorFinal: data.valorFinal,
+            car: {
+                placa: data.placa,
+                modelo: data.modelo,
+                cor: data.cor
+            }
+        }
+        res.status(201).send({ status: 1, data: formatedDate })
+    } catch (error) {
+        res.status(500).send({ status: 0, msg: 'Algo deu errado.', error: error })
+    }
+}
+
+exports.selectCheckOut = async (req, res) => {
+    try {
+        const id = req.params.id
+        const snapshot = await parking.doc(id).get()
+        const data = snapshot.data()
+        const formatedDate = {
+            id: id,
+            hrEntrada: new Date(data.hrEntrada.seconds * 1000).toLocaleString('pt-BR', {
+                timeZone: 'America/Sao_Paulo'
+            }),
+            hrSaida: new Date(data.hrSaida.seconds * 1000).toLocaleString('pt-BR', {
+                timeZone: 'America/Sao_Paulo'
+            }),
+            isParking: data.isParking,
+            emailFuncionario: data.emailFuncionario,
+            valorFinal: data.valorFinal,
+            car: {
+                placa: data.placa,
+                modelo: data.modelo,
+                cor: data.cor
+            }
+        }
+        res.status(201).send({ status: 1, data: formatedDate })
     } catch (error) {
         res.status(500).send({ status: 0, msg: 'Algo deu errado.', error: error })
     }
--- src/routes/parking.js
+++ src/routes/parking.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router()
 
-const { checkIn, listarCheckIn, checkOut, listarCheckOut } = require('../controllers/parking')
+const { checkIn, listarCheckIn, checkOut, listarCheckOut, selectCheckIn, selectCheckOut} = require('../controllers/parking')
 
 router.post("/checkIn", checkIn)
 router.get("/listarCheckIn", listarCheckIn)
 router.get("/listarCheckOut", listarCheckOut)
 router.post('/checkOut/:id', checkOut)
+router.get('/selectCheckIn/:id', selectCheckIn)
+router.get('/selectCheckOut/:id', selectCheckOut)
 
 module.exports = router;
