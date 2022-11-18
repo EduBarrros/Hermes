47d25f70d3985fff665d196d2160bdb293ddb021
EduardoEshows <eduardo.barros@eshows.com.br>
2022-11-17 21:06:39 -0300

    Feat: add new types of reports

--- src/controllers/report.js
+++ src/controllers/report.js
@@ -9,6 +9,11 @@ const db = firebase.firestore()
 const parking = db.collection('cl_parking')
 
 exports.flowReport = async (req, res) => {
+
+    const StartDate = req.body.start
+    const EndDate = req.body.end
+    const Type = req.body.type
+
     try {
 
         const html = fs.readFileSync(path.resolve(__dirname, "../public/template.html"), 'utf-8')
@@ -36,19 +41,45 @@ exports.flowReport = async (req, res) => {
             }
         };
 
-        const snapshot = await parking.where('hrSaida', '!=', null).get()
-        const listParking = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
-        const parkings = listParking.map((doc) => ({
+        var snapshot = []
+        var listParking = []
+
+        if (Type === 1) {
+
+            snapshot = await parking.where('hrSaida', '!=', null).get()
+            listParking = await snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+
+        } else if (Type === 2 && StartDate !== undefined && EndDate !== undefined) {
+
+            const StartParseDate = firebase.firestore.Timestamp.fromDate(new Date(StartDate));
+
+            const EndParseDate = firebase.firestore.Timestamp.fromDate(new Date(EndDate));
+
+            snapshot = await parking.where('hrSaida', '>', StartParseDate).where('hrSaida', '<', EndParseDate).get()
+            listParking = await snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+
+        } else if (Type === 3 && StartDate !== undefined && EndDate !== undefined) {
+
+            const StartParseDate = firebase.firestore.Timestamp.fromDate(new Date(StartDate));
+
+            const EndParseDate = firebase.firestore.Timestamp.fromDate(new Date(EndDate));
+
+            snapshot = await parking.where('hrEntrada', '>', StartParseDate).where('hrEntrada', '<', EndParseDate).get()
+            listParking = await snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+
+        } else {
+            res.status(404).send({ status: 0, msg: 'Não foram passados os parametros necessários para gerar o relatório.' })
+        }
+
+        const parkings = await listParking.map((doc) => ({
             id: doc.id,
             hrEntrada: new Date(doc.hrEntrada.seconds * 1000).toLocaleString('pt-BR', {
                 timeZone: 'America/Sao_Paulo'
             }),
-            hrSaida: new Date(doc.hrSaida.seconds * 1000).toLocaleString('pt-BR', {
-                timeZone: 'America/Sao_Paulo'
-            }),
+            hrSaida: !doc.hrSaida ? 'Ainda estacionado' : new Date(doc.hrSaida.seconds * 1000).toLocaleString('pt-BR', {timeZone: 'America/Sao_Paulo'}),
             isParking: doc.isParking,
             emailFuncionario: doc.emailFuncionario,
-            valorFinal: doc.valorFinal,
+            valorFinal: !doc.valorFinal ? 0 : doc.valorFinal,
             car: {
                 placa: doc.placa,
                 modelo: doc.modelo,
@@ -91,7 +122,7 @@ exports.flowReport = async (req, res) => {
                 }
             )
 
-        res.status(201).send({ status: 1, msg: 'Relatório gerado com sucesso', report: report64})
+        res.status(201).send({ status: 1, msg: 'Relatório gerado com sucesso', report: report64 })
     } catch (error) {
         res.status(500).send({ status: 0, msg: 'Algo deu errado.', error: error })
     }
